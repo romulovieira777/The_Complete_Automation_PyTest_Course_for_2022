5850052bc0a094b75f93f430aab486342bee1bf4
Romulo Vieira <romulo.vieira777@gmail.com>
2023-03-13 21:23:45 -0300

    Writing Test Functions

---
+++ Section_05_Writing_Test_Functions/PyTest/tests/unitTest/test_task_fail.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 13, 2023
+
+@author: Romulo
+'''
+
+from tasks import Task
+
+
+def test_task_equility():
+    t1 = Task('sit there', 'james')
+    t2 = Task('do something', 'smith')
+    assert t1 == t2
+
+
+def test_dict_equality():
+    t1_dict = Task('make sandwich', 'smith')._asdict()
+    t2_dict = Task('make sandwich', 'smit')._asdict()
+    assert t1_dict == t2_dict
